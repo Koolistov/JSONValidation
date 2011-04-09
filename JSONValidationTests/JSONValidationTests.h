f82cb5f6e96b0236afc477cab5d0fa1b0d680d00
Johan Kool <johankool@gmail.com>
2011-04-10 00:12:47 +0800

    Initial Commit

---
+++ JSONValidationTests/JSONValidationTests.h
@@ -0,0 +1,17 @@
+//
+//  JSONValidationTests.h
+//  JSONValidationTests
+//
+//  Created by Johan Kool on 10-04-2011.
+//  Copyright 2011 Koolistov. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+
+@interface JSONValidationTests : SenTestCase {
+@private
+    
+}
+
+@end
---
+++ JSONValidationTests/JSONValidationTests.m
@@ -0,0 +1,33 @@
+//
+//  JSONValidationTests.m
+//  JSONValidationTests
+//
+//  Created by Johan Kool on 10-04-2011.
+//  Copyright 2011 Koolistov. All rights reserved.
+//
+
+#import "JSONValidationTests.h"
+
+
+@implementation JSONValidationTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in JSONValidationTests");
+}
+
+@end
